565f6ddc0ce94fdc32560e9c50c521108de9a808
Soutif Thomas <th.soutif@gmail.com>
2021-01-15 13:04:18 +0100

    création du dossier class correspondant à la couché metier

--- src/database/DAO/DAO.java
+++ src/database/DAO/DAO.java
@@ -7,6 +7,26 @@ public interface DAO<T> {
 
     public Connection conn = ConnexionPostGreSQL.getInstance().getConnection();
 
+    /**
+     * Permet la suppression d'un tuple de la base
+     *
+     * @param obj
+     */
+    boolean delete(T obj);
 
+    /**
+     * Permet de créer une entrée dans la base de données par rapport à un objet
+     *
+     * @param obj
+     */
+    T insert(T obj);
+
+    /**
+     * Permet de mettre à jour les données d'un tuple dans la base à partir d'un
+     * objet passé en paramètre
+     *
+     * @param obj
+     */
+    boolean update(T obj);
 
 }
