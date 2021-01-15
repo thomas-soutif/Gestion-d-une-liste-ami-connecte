39431f3dd82d9a801eb93fb5f6c1622a68da8573
Soutif Thomas <th.soutif@gmail.com>
2021-01-15 15:52:19 +0100

    Implémentation des methodes héritées dans FriendRelationDAO et FriendRequestDAO

--- src/database/DAO/FriendRelationDAO.java
+++ src/database/DAO/FriendRelationDAO.java
@@ -1,6 +1,34 @@
 package database.DAO;
 
-public class FriendRelationDAO{
+import database.CLASSES.FriendRelation;
+
+import java.util.List;
+
+public class FriendRelationDAO implements IFriendRelationDAO{
 
 
+    @Override
+    public List<FriendRelation> getAllFriendsOfUser() {
+        return null;
+    }
+
+    @Override
+    public boolean haveFriendRelation() {
+        return false;
+    }
+
+    @Override
+    public boolean delete(FriendRelation obj) {
+        return false;
+    }
+
+    @Override
+    public FriendRelation insert(FriendRelation obj) {
+        return null;
+    }
+
+    @Override
+    public boolean update(FriendRelation obj) {
+        return false;
+    }
 }
--- src/database/DAO/FriendRequestDAO.java
+++ src/database/DAO/FriendRequestDAO.java
@@ -1,6 +1,29 @@
 package database.DAO;
 
-public class FriendRequestDAO{
+import database.CLASSES.FriendRequest;
+
+import java.util.List;
+
+public class FriendRequestDAO implements IFriendRequestDAO {
 
 
+    @Override
+    public List<FriendRequest> getFriendRequestsOfUser() {
+        return null;
+    }
+
+    @Override
+    public boolean delete(FriendRequest obj) {
+        return false;
+    }
+
+    @Override
+    public FriendRequest insert(FriendRequest obj) {
+        return null;
+    }
+
+    @Override
+    public boolean update(FriendRequest obj) {
+        return false;
+    }
 }
