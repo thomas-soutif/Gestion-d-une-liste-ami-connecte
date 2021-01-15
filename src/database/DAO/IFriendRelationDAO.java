d046b5c6ad19ed07299a87cf9009e8d256792f4b
Soutif Thomas <th.soutif@gmail.com>
2021-01-15 15:51:44 +0100

    Ajout des définitions des méthodes dans les interfaces IFriendRelationDAO et IFriendRequestDAO

--- src/database/DAO/IFriendRelationDAO.java
+++ src/database/DAO/IFriendRelationDAO.java
@@ -1,9 +1,13 @@
 package database.DAO;
 
-public interface IFriendRelationDAO{
+import database.CLASSES.FriendRelation;
 
+import java.util.List;
 
+public interface IFriendRelationDAO extends DAO<FriendRelation> {
 
+    List<FriendRelation> getAllFriendsOfUser(); // TODO: Ajouter en paramètre un objet User (user : User)
+    boolean haveFriendRelation(); // TODO: Ajouter en paramètre deux objets User (user1 : User, user2 : User)
 
 
 }
--- src/database/DAO/IFriendRequestDAO.java
+++ src/database/DAO/IFriendRequestDAO.java
@@ -1,8 +1,11 @@
 package database.DAO;
 
-public interface  IFriendRequestDAO{
+import database.CLASSES.FriendRequest;
 
+import java.util.List;
 
+public interface  IFriendRequestDAO extends DAO<FriendRequest> {
 
+    List<FriendRequest> getFriendRequestsOfUser(); // TODO: Ajouter en paramètre un objet user (user : User)
 
 }
