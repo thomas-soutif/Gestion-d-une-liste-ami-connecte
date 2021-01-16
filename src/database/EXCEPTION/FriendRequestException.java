316455c930182c7eb543b198d38e5c3a47731d1e
Soutif Thomas <th.soutif@gmail.com>
2021-01-16 13:11:57 +0100

    Ajout d'une gestion des exceptions personnalisées

---
+++ src/database/EXCEPTION/FriendRequestException.java
@@ -0,0 +1,34 @@
+package database.EXCEPTION;
+
+public class FriendRequestException extends Exception {
+
+    private  final ErrorType code;
+    public FriendRequestException(ErrorType code){
+        super();
+        this.code = code;
+    }
+
+    public FriendRequestException(String message, Throwable cause, ErrorType code){
+        super(message,cause);
+        this.code = code;
+    }
+    public FriendRequestException(String message, ErrorType code){
+        super(message);
+        this.code = code;
+    }
+
+    public FriendRequestException(Throwable cause, ErrorType code){
+        super(cause);
+        this.code = code;
+    }
+
+    @Override
+    public synchronized Throwable getCause() {
+        return super.getCause();
+    }
+
+    public ErrorType getErrorType() {
+        return this.code;
+    }
+
+}
