b9ff82c5b6d557357dc8c3e41c058c1c7961a36b
Soutif Thomas <th.soutif@gmail.com>
2021-01-16 13:19:45 +0100

    ajout attribut dans la table UserTemp

--- src/database/CLASSES/UserTemp.java
+++ src/database/CLASSES/UserTemp.java
@@ -1,5 +1,10 @@
 package database.CLASSES;
 
 public class UserTemp {
+    private Integer id;
+    private String name;
+    private String firstName;
+    private String pseudo;
+    private String password0;
 
 }
