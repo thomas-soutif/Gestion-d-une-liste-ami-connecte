a1ad61d9feedc4e0c63eb480449c1705251ca8d2
Jean-Michel dossou <jean-michel.dossou@etu.univ-lyon1.fr>
2021-01-23 13:06:54 +0100

    Création de la classe User (AccountUser) et mise en place des extends et impémnents entre les différentes interfaces et classes
    Ajout des attributs à la classe FriendRelation
    
    Ajout des attributs à la classe FriendRequest

--- src/database/CLASSES/FriendRelation.java
+++ src/database/CLASSES/FriendRelation.java
@@ -1,4 +1,45 @@
 package database.CLASSES;
 
+import java.util.Date;
+
 public class FriendRelation {
+
+    private int id;
+    private String from_user;
+    private String to_user;
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFrom_user() {
+        return from_user;
+    }
+
+    public String getTo_user() {
+        return to_user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public void setFrom_user(String from_user) {
+        this.from_user= from_user;
+    }
+
+    public void setTo_user(String to_user) {
+        this.to_user = to_user;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 }
--- src/database/CLASSES/FriendRequest.java
+++ src/database/CLASSES/FriendRequest.java
@@ -1,4 +1,34 @@
 package database.CLASSES;
 
 public class FriendRequest {
+
+    private int id;
+    private String user_1;
+    private String user_2;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUser_1() {
+        return user_1;
+    }
+
+    public String getUser_2() {
+        return user_2;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public void setUser_1(String user_1) {
+        this.user_1= user_1;
+    }
+
+    public void setUser_2(String user_2) {
+        this.user_2 = user_2;
+    }
+
 }
