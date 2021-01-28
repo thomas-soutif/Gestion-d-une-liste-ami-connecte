33947eed8a3c7988ee626f627949510a4752e2ca
Soutif Thomas <th.soutif@gmail.com>
2021-01-28 09:52:58 +0100

    fonction pour récupérer la liste des users dans la base de données

--- src/database/DAO/UserDAO.java
+++ src/database/DAO/UserDAO.java
@@ -1,7 +1,12 @@
 package database.DAO;
 
 import database.CLASSES.AccountUser;
+import database.CLASSES.FriendRelation;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAO implements IUserDAO {
@@ -9,7 +14,28 @@ public class UserDAO implements IUserDAO {
 
     @Override
     public List<AccountUser> getAccountOfUsers() {
-        return null;
+        List<AccountUser> list= new ArrayList<>();
+
+        try{
+            Statement database_instance= conn.createStatement();
+            ResultSet result = database_instance.executeQuery("SELECT * FROM account_user ORDER BY id");
+
+            while(result.next()){
+                AccountUser account_user = new AccountUser();
+
+                account_user.setId(result.getInt("id"));
+                account_user.setFirstName(result.getString("firstname"));
+                account_user.setName(result.getString("name"));
+                account_user.setPassword(result.getString("password"));
+
+                list.add(account_user);
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+
+
+        return list;
     }
 
     @Override
