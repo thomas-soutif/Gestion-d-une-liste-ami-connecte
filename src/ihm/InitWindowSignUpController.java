766fd9e71ce529f54e63648185b5bf7fc180164b
Jean-Michel dossou <jean-michel.dossou@etu.univ-lyon1.fr>
2021-03-04 18:48:34 +0100

    Création de la classe User (AccountUser) et mise en place des extends et impémnents entre les différentes interfaces et classes
    Ajout des attributs à la classe FriendRelation
    
    Ajout des attributs à la classe FriendRequest
    
    Création de la classe UserDAO
    
    Commit de nouveau avec push
    
    Commit et correctifs importants apportés au dernier push
    
    Création des scènes se "connecter", "connexion" et "s'inscrire", en passe d'une fenêtre à une autre par une méthode de switch.
    
    Premiers tests sur l'interface connexion erreurs à corriger.
    
    Création des contrôleurs pour les boutons "se connecter" et "s'inscrire".
    
    Correction de la scène InitWindow.fxml et test de l'interface
    
    Correction des interactions entre les différentes interfaces
    
    Création des TODo à faire pour finir les tests de récupérations des données (MDP)
    
    Envoi des parties modifiées
    
    Correction du bug sur les interfaces FXML
    
    Création des conditions de vérifications des champs de saisies lors de l'inscription.

--- src/ihm/InitWindowSignUpController.java
+++ src/ihm/InitWindowSignUpController.java
@@ -7,12 +7,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import network.Client.Handler.HandlerAuthClient;
 
 import java.io.IOException;
 
 public class InitWindowSignUpController {
 
     public Hyperlink hyperLinkBackToConnection;
+    public TextField textFieldPseudo;
+    public TextField textFieldPrenom;
+    public TextField textFieldNom;
+    public PasswordField passwordFieldMdp;
+    public PasswordField passwordFieldConfirmMdp;
+
+
     @FXML
     private Button buttonSignUp;
 
@@ -30,6 +40,18 @@ public class InitWindowSignUpController {
     private void hyperLinkBackToConnection(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("InitWindowConnection.fxml"));
         InterfaceClient.getMainStage().setScene(new Scene(root));
+    }
+
+    public void ActionSignUp(ActionEvent actionEvent) {
+        String mdpConfirmation = passwordFieldConfirmMdp.getText();
+        String mdp = passwordFieldMdp.getText();
+        if (textFieldPseudo.getText().isBlank()) {
+            //TODO afficher message d'erreur
+            //TODO seconde condition, mettre un else if pour comparer si
+        }
+       else if (mdpConfirmation.equals(mdp)){
+            HandlerAuthClient.handlerUserInscriptionRequest(textFieldPseudo.getText(), mdp, textFieldPrenom.getText(), textFieldNom.getText());
+        }
     }
 }
 //TODO récupérer les valeurs des champs et confirmer les inscriptions!
